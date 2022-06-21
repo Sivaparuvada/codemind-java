6a7fc7a1cca3a591978f6926afe946d3877369ac
Sivaparuvada <96334378+Sivaparuvada@users.noreply.github.com>
2022-06-21 21:59:23 +0530

    import java.util.Scanner; class temp {     public static void main(String args[])     {         int c;         float f;         Scanner sc=new Scanner(System.in);         c=sc.nextInt();         f=(float)(32+(c*1.8));         System.out.format("%.2f",f);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class temp
+{
+    public static void main(String args[])
+    {
+        int c;
+        float f;
+        Scanner sc=new Scanner(System.in);
+        c=sc.nextInt();
+        f=(float)(32+(c*1.8));
+        System.out.format("%.2f",f);
+    }
+}
